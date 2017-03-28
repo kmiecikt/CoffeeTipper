32403d9e9f12d7bf2b0c8a4eb75efc693aca199d
Tomasz Kmiecik <tomaskmi@microsoft.com>
2017-03-28 16:19:29 +0200

    Testing Xamarin Android postprocess script

---
+++ Scripts/android-xamarin-postprocess.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+if [ -z "$1" ]; then
+  echo "No pattern specified."
+  exit 0
+fi
+
+pattern=$1
+files=( $pattern )
+
+echo "Removing all ABI APKs except the Universal APK…"
+i=0
+
+for file in `ls $pattern`
+do
+  if [ $i -gt 0 ]; then
+    # rm $FILE
+    echo "  Removed APK $file"
+  fi
+  i=$((i+1))
+done
